8a0761789dd41d403d4f51d03035b3d70366f7a4
EarsAndEggs <containmyspam@outlook.com>
2018-07-23 05:11:59 +0100

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -17,11 +17,20 @@ after_initialize do
   User.register_custom_field_type('signature_raw', :text)
 
   if SiteSetting.signatures_enabled then
-    add_to_serializer(:post, :user_signature, true) {
+    add_to_serializer(:post, :user_signature, false) {
       if SiteSetting.signatures_advanced_mode then
         object.user.custom_fields['signature_cooked'] if object.user
       else
         object.user.custom_fields['signature_url'] if object.user
+      end
+    }
+
+    # I guess this should be the default @ discourse. PR maybe?
+    add_to_serializer(:user, :custom_fields, false) {
+      if object.custom_fields == nil then
+        {}
+      else
+        object.custom_fields
       end
     }
   end
